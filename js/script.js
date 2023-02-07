6946bc788ccc984c020e1b3cb2dbd02ad898b424
Никитюк В.А <NikitukVA@kuzlitmash.org>
2023-02-07 13:53:16 +0300

    init: fetch asaync await

--- js/script.js
+++ js/script.js
@@ -105,9 +105,9 @@
           modalCloseBtn=document.querySelector('[data-close]');
     
     function openModal(){
-        modal.classList.toggle('show');
-        //     modal.classList.add('show');
-        //     modal.classList.remove('hide');
+       // modal.classList.toggle('show');
+            modal.classList.add('show');
+            modal.classList.remove('hide');
             document.body.style.overflow='hidden'; // чтобы не листать страницу пока мы в модальном окне
         clearInterval(modalTimerId);
         }
@@ -122,10 +122,10 @@
         btn.addEventListener('click', openModal);
     });
 
-    modalCloseBtn.addEventListener('click',closeModal);
+   // modalCloseBtn.addEventListener('click',closeModal);
 
     modal.addEventListener('click',(event)=>{ // выход из модального окна по щелчку вне его
-        if (event.target === modal ){
+        if (event.target === modal || event.target.getAttribute('data-close')==''){
             closeModal();
         }
     })
@@ -140,11 +140,11 @@
 
     //Additional Modal Functions
     
-  // let modalTimerId=setInterval(openModal,4000)
+    let modalTimerId=setInterval(openModal,40000)
   
     
     function scrollModal(){  
-            console.log( 'Текущая прокрутка сверху: ' + window.pageYOffset +' '+ document.documentElement.scrollHeight +' '+ document.documentElement.clientHeight );
+          //  console.log( 'Текущая прокрутка сверху: ' + window.pageYOffset +' '+ document.documentElement.scrollHeight +' '+ document.documentElement.clientHeight );
             if(window.pageYOffset+document.documentElement.clientHeight >= 
                 document.documentElement.scrollHeight) //
             { openModal();
@@ -153,14 +153,15 @@
         }
     window.addEventListener('scroll',scrollModal); 
     
-
+// add new El
 class Menu{
-    constructor(src, alt,title, descr, price,parentSelector){
+    constructor(src, alt,title, descr, price,parentSelector, ...classes){
         this.src=src;
         this.alt=alt;
         this.title=title;
         this.descr=descr;
         this.price=price;
+        this.classes=classes; // array
         this.parent=document.querySelector(parentSelector)
         this.transfer=10;
         this.convertToBlr()
@@ -172,30 +173,220 @@ class Menu{
     }
     render(){
         let element=document.createElement('div');
+
+        if(this.classes.length===0){
+            this.classes='menu__item';
+            element.classList.add(this.classes);
+        }
+        else{
+            this.classes.forEach(className=> element.classList.add(className));
+        }
             element.innerHTML=`
-            <div class="menu__item">
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-            </div>
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>`;
         this.parent.append(element);
+        }
     }
-}
 
 // let div=new Menu();
 // div.render();
-new Menu(
-    "img/tabs/arbuz.jpg",
-     "arbuz is here",
-     "Меню - арбуз", 
-     "Сочный сладкий кусочек блаженства в жаркий летний вечер, который подогреет атмосферу в любом удобном для вас месте. Отлично сочтается с бананом", 
-     12,
-     '.menu .container').render();
+
+
+let getResource=async (url)=>{
+    let res=await fetch(url);
     
+    if(!res.ok){
+       throw new Error(`Noo fetch ${url}, status: ${res.status}`);
+    }
+    return await res.json();
+};
+
+getResource('http://localhost:3000/menu')
+.then(data=> createCard(data));
+
+function createCard(data){
+    data.forEach(({img, altimg,title,descr,price})=>{  // destruktur-et object
+        let element=document.createElement('div');
+        element.classList.add('menu__item');
+        element.innerHTML=`
+        <img src=${img} alt=${altimg}>
+        <h3 class="menu__item-subtitle">${title}</h3>
+        <div class="menu__item-descr">${descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${price}</span> грн/день</div>
+        </div>`;
+        document.querySelector('.menu .container').append(element);
+    })
+}
+
+
+// getResource('http://localhost:3000/menu')
+// .then(data=>{
+//     data.forEach(({img, altimg,title,descr,price})=>{
+//         new Menu(img, altimg,title,descr,price, '.menu .container' ).render(); // деструктур объекта
+//     })
+// })
+
+// new Menu(
+//     "img/tabs/arbuz.jpg",
+//     "arbuz is here",
+//     "Меню - арбуз", 
+//     "Сочный сладкий кусочек блаженства в жаркий летний вечер, который подогреет атмосферу в любом удобном для вас месте. Отлично сочтается с бананом", 
+//     12,
+//     '.menu .container',
+//     'menu__item').render();
+// new Menu(
+//     "img/tabs/vegy.jpg",
+//     "arbuz is here",
+//     "Меню - арбуз", 
+//     "Сочный сладкий кусочек блаженства в жаркий летний вечер, который подогреет атмосферу в любом удобном для вас месте. Отлично сочтается с бананом", 
+//     12,
+//     '.menu .container',
+//     'menu__item',
+//     'big').render();
+
+
+      //  forms
+    let forms=document.querySelectorAll('form');
+    let message={
+        loading: 'Загрузка',
+        success: 'Мы с вами свяжемся',
+        failure: 'Сбой произошёл...'
+    };
+    forms.forEach(item=>{
+        bindPostData(item);
+    })
+    
+    let postData=async (url,data)=>{
+        let res=await fetch(url,{
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+        return await res.json();
+
+    }
+
+
+    function bindPostData(form) {
+        form.addEventListener('submit',(event)=>{
+            event.preventDefault();
+            
+            let statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
+         
+           /* let request=new XMLHttpRequest();
+            request.open('POST','server.php'); */
+           // request.setRequestHeader('Content-type', 'multipart/form-data');
+           // request.setRequestHeader('Content-type', 'application/json')
+            
+           let formData=new FormData(form);
+
+            console.log(Object.fromEntries(formData.entries()))
+           
+            // let object={};
+            // formData.forEach((value,key)=>{
+            //     object[key]=value;
+            // });
+            // вместо выше
+            let json=JSON.stringify(Object.fromEntries(formData.entries()));
+            // formData превр-ем в массив массивов, затем в классич объект, после в JSON
+            
+
+
+            //request.send(json);
+
+            //request.send(formData); 
+
+
+            // fetch('server.php',{
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-type': 'application/json'
+            //     },
+            //     body: JSON.stringify(object)
+            // })
+            postData('http://localhost:3000/requests', json) //JSON.stringify(object)
+          //  .then(data=>data.text())
+            .then (data=>{
+                    console.log(data);
+                    //statusMessage.textContent=message.success;
+                    showThanksModal(message.success);
+                    form.reset(); // очищает поля ввода form.reset
+                    statusMessage.remove();
+            }).catch(()=>{
+                console.log('bad');
+                //statusMessage.textContent=message.failure;
+                showThanksModal(message.failure);
+            }).finally(()=>{
+                form.reset();
+            });
+
+            // request.addEventListener('load',()=>{
+            //     if(request.status=== 200){
+            //         console.log(request.response);
+            //         //statusMessage.textContent=message.success;
+            //         showThanksModal(message.success);
+            //          // очищает поля ввода form.reset
+            //         form.reset();
+            //         statusMessage.remove();
+            //         // setTimeout(()=>{ 
+            //         //     statusMessage.remove();
+            //         //      } , 4000);            
+            //     }
+            //     else {
+            //         console.log('bad');
+            //         // statusMessage.textContent=message.failure;
+            //         showThanksModal(message.failure);
+            //     }
+            // });
+        });
+    }
+
+
+
+   function showThanksModal(message){
+        
+        let prevModalDialog=document.querySelector('.modal__dialog');
+        prevModalDialog.classList.add('hide'); // скроет модальное окно с данными юзера
+        
+        openModal();// откроет модальные окна
+        let thanksModal=document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML=`
+        <div class='modal__content'>
+            <div class='modal__close' data-close>&times;</div>
+            <div class ='modal__title'>${message}</div>
+        </div>
+        `;
+       
+       document.querySelector('.modal').append(thanksModal);
+
+       setTimeout(()=>{
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+       }, 3000);
+    }
+
+    fetch('http://localhost:3000/menu')
+    .then(data=>data.json())
+    .then(res=>console.log(res));
 
 //})
