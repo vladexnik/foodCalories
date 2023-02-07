3c847eb9977b9bfd304b7f0ac65be4e2554738c4
Никитюк В.А <NikitukVA@kuzlitmash.org>
2023-02-07 15:17:46 +0300

    update: axios add data

--- js/script.js
+++ js/script.js
@@ -214,35 +214,47 @@ let getResource=async (url)=>{
     return await res.json();
 };
 
-// getResource('http://localhost:3000/menu')
-// .then(data => {
-//     data.forEach(({img, altimg, title, descr, price}) => {
+getResource('http://localhost:3000/menu')
+.then(data => {
+    data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+    });
+});
+
+
+// axios.get('http://localhost:3000/menu') // библиотека, отличие от фетча - возвр более подробный ответ и все данные в св-ве data
+//     .then(datta=>{
+//         datta.data.forEach(({img, altimg, title, descr, price}) => {
 //         new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+//         });
 //     });
-// });
 
 
- getResource('http://localhost:3000/menu')
-     .then(data => createCard(data));
-function createCard(data) {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            const element = document.createElement('div');
 
-            element.classList.add("menu__item");
+// getResource('http://localhost:3000/menu')
+//      .then(data => createCard(data));
+// function createCard(data) {
+//         data.forEach(({img, altimg, title, descr, price}) => {
+//             const element = document.createElement('div');
 
-            element.innerHTML = `
-                <img src=${img} alt=${altimg}>
-                <h3 class="menu__item-subtitle">${title}</h3>
-                <div class="menu__item-descr">${descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${price}</span> грн/день</div>
-                </div>
-            `;
-            document.querySelector(".menu .container").append(element);
-        });
-    }
+//             element.classList.add("menu__item");
+
+//             element.innerHTML = `
+//                 <img src=${img} alt=${altimg}>
+//                 <h3 class="menu__item-subtitle">${title}</h3>
+//                 <div class="menu__item-descr">${descr}</div>
+//                 <div class="menu__item-divider"></div>
+//                 <div class="menu__item-price">
+//                     <div class="menu__item-cost">Цена:</div>
+//                     <div class="menu__item-total"><span>${price}</span> грн/день</div>
+//                 </div>
+//             `;
+//             document.querySelector(".menu .container").append(element);
+//         });
+//     }
+
+
+
 
 // new MenuCard(
 //     "img/tabs/arbuz.jpg",
